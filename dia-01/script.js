6340f67ae8ea339212a9053d8e9540a307c37817
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-07-28 10:38:08 -0300

    dia01

---
+++ dia-01/script.js
@@ -0,0 +1,20 @@
+let titulo = document.getElementById("titulo");
+
+// titulo.style.color = "blue";
+
+// let oMelhorFilme = document.getElementById("obraprima");
+// let listaDeFilmes = document.getElementsByTagName("p");
+
+// oMelhorFilme.style.color = "red";
+
+// listaDeFilmes.style.color = "red";
+
+// console.log(listaDeFilmes[0].innerText);
+
+// let melhorDeTodos = listaDeFilmes[0];
+
+// melhorDeTodos.style.color = "red";
+
+let lista = document.querySelectorAll("p")
+
+console.log(lista)
